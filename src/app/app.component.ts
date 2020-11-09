2791f80a2e67a36ae24bbc8c4c4a561c6145ba8f
ronnelreposo <ronnel.reposo@gmail.com>
2020-11-09 16:51:05 +0800

    Map All

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,7 +7,8 @@ import {
   pipe,
   Observable,
   Scheduler,
-  defer
+  defer,
+  EMPTY
 } from "rxjs";
 import { animationFrame } from "rxjs/internal/scheduler/animationFrame";
 import {
@@ -27,12 +28,13 @@ import {
 } from "rxjs/operators";
 
 type Point = { x: number; y: number };
+type TransitionViewState = {
+  color: string;
+  position: Point;
+};
 type Item = {
   id: number;
-  transitionViewState: {
-    color: string;
-    position: Point;
-  };
+  transitionViewState: TransitionViewState;
 };
 type Data = {
   items: Item[];
@@ -60,6 +62,7 @@ export class AppComponent implements OnInit {
         ? 4.0 * t * t * t
         : 0.5 * Math.pow(2.0 * t - 2.0, 3.0) + 1.0;
     };
+    const distance = (d: number) => (t: number) => d * t;
 
     const initPoint: Point = { x: 0, y: 0 };
     const initialData: Data = {
@@ -79,44 +82,98 @@ export class AppComponent implements OnInit {
       ]
     };
 
-    this.data$ = this.runTransitionSubj.pipe(
-      switchMap(id => of(initialData)),
-      startWith(initialData)
-    );
-
-    const target$ = fromEvent(document, "mousedown").pipe(map(toPoint));
-
-    let ball = document.getElementById("ball");
-
+    // HOD Observables.
     const msElapsed$ = interval(1, animationFrame);
-
-    // duration in percentage.
     const duration$ = (ms: number) =>
       msElapsed$.pipe(
         map(ems => ems / ms),
         takeWhile(t => t <= 1)
       );
 
-    const durationMs = 300;
-    const distance = (d: number) => (t: number) => d * t;
+    // deltas ----------------------------------------------------
+    const deltaPoint = ({ x, y }: Point, t: number): Point => {
+      return {
+        x: distance(x)(t),
+        y: distance(y)(t)
+      };
+    };
+    const updateItemPositions = (targetPoint: Point) => (data: Data) => (
+      t: number
+    ): Data => {
+      const newItems: Item[] = data.items.map(item => {
+        const newTransitionViewState: TransitionViewState = {
+          ...item.transitionViewState,
+          position: deltaPoint(targetPoint, t)
+        };
 
-    const s$ = target$.pipe(
-      switchMap(({ x, y }) =>
-        duration$(durationMs).pipe(
-          map(cubicInOut),
-          map(t => ({
-            x: distance(x)(t),
-            y: distance(y)(t)
-          }))
-        )
-      )
+        return { ...item, transitionViewState: newTransitionViewState };
+      });
+
+      return {
+        ...data,
+        items: newItems
+      };
+    };
+
+    // TODO. abstract more.
+    const f = (data: Data, durationMs: number, targetPoint: Point) => (
+      id: number
+    ): Observable<Data> => {
+      // mapping all items.
+      const data$ = duration$(durationMs).pipe(
+        map(cubicInOut),
+        map(updateItemPositions(targetPoint)(data))
+      );
+
+      // find in the data, minimal.
+      // const items$ = data.items.map(item => {
+
+      //   if (item.id === id) {
+      //     return duration$(durationMs).pipe(
+      //       map(cubicInOut),
+      //       map(t => ({
+      //         x: distance(target.x)(t),
+      //         y: distance(target.y)(t)
+      //       }))
+      //     )
+      //   } else {
+      //     return EMPTY;
+      //   }
+      // });
+
+      return data$;
+    };
+
+    const target: Point = { x: 400, y: 0 };
+    const durationMs = 300;
+    this.data$ = this.runTransitionSubj.pipe(
+      switchMap(f(initialData, durationMs, target)),
+      startWith(initialData)
     );
 
-    this.out$ = s$;
+    // const target$ = fromEvent(document, "mousedown").pipe(map(toPoint));
+
+    // let ball = document.getElementById("ball");
+
+    // duration in percentage.
+
+    // const s$ = target$.pipe(
+    //   switchMap(({ x, y }) =>
+    //     duration$(durationMs).pipe(
+    //       map(cubicInOut),
+    //       map(t => ({
+    //         x: distance(target.x)(t),
+    //         y: distance(target.y)(t)
+    //       }))
+    //     )
+    //   )
+    // );
+
+    // this.out$ = s$;
 
     // when done, simply call sub.unsubscribe();
-    let sub = s$.subscribe(({ x, y }) => {
-      ball.style.transform = `translate3d(${x}px, ${y}px, ${0}px)`;
-    });
+    // let sub = s$.subscribe(({ x, y }) => {
+    //   ball.style.transform = `translate3d(${x}px, ${y}px, ${0}px)`;
+    // });
   }
 }
