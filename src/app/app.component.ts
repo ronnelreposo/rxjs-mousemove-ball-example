80838f34504fb2bbf3615a9773972852fe16d2d4
ronnelreposo <ronnel.reposo@gmail.com>
2020-11-09 21:11:32 +0800

    Multi Target and Trigger, Single Stage

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -24,21 +24,29 @@ import {
   mapTo,
   takeWhile,
   mergeMap,
-  startWith
+  startWith,
+  withLatestFrom
 } from "rxjs/operators";
+import * as _ from "lodash";
 
 type Point = { x: number; y: number };
 type TransitionViewState = {
   color: string;
   position: Point;
+  percentage: number;
 };
 type Item = {
   id: number;
   transitionViewState: TransitionViewState;
 };
+type IdAndPercentage = { id: number; t: number };
 type Data = {
   items: Item[];
 };
+
+// Helper Types.
+type LogEntry = { id: number; eventType: "click" };
+type Logs = { counter: number; logs: LogEntry[] };
 
 @Component({
   selector: "my-app",
@@ -50,6 +58,8 @@ export class AppComponent implements OnInit {
 
   data$: Observable<Data>;
   runTransitionSubj: Subject<number>;
+
+  logs$: Observable<Logs>;
 
   ngOnInit() {
     // Represents Dispatch.
@@ -65,24 +75,37 @@ export class AppComponent implements OnInit {
     const distance = (d: number) => (t: number) => d * t;
 
     const initPoint: Point = { x: 0, y: 0 };
+    const targetPoint: Point = { x: 400, y: 0 };
     const initialData: Data = {
       items: [
         {
           id: 1,
-          transitionViewState: { color: "#c0392b", position: initPoint }
+          transitionViewState: {
+            color: "#c0392b",
+            position: initPoint,
+            percentage: 0
+          }
         },
         {
           id: 2,
-          transitionViewState: { color: "#2ecc71", position: initPoint }
+          transitionViewState: {
+            color: "#2ecc71",
+            position: initPoint,
+            percentage: 0
+          }
         },
         {
           id: 3,
-          transitionViewState: { color: "#f1c40f", position: initPoint }
+          transitionViewState: {
+            color: "#f1c40f",
+            position: initPoint,
+            percentage: 0
+          }
         }
       ]
     };
 
-    // HOD Observables.
+    // HO Observables.
     const msElapsed$ = interval(1, animationFrame);
     const duration$ = (ms: number) =>
       msElapsed$.pipe(
@@ -97,83 +120,68 @@ export class AppComponent implements OnInit {
         y: distance(y)(t)
       };
     };
-    const updateItemPositions = (targetPoint: Point) => (data: Data) => (
-      t: number
-    ): Data => {
-      const newItems: Item[] = data.items.map(item => {
-        const newTransitionViewState: TransitionViewState = {
-          ...item.transitionViewState,
-          position: deltaPoint(targetPoint, t)
-        };
 
-        return { ...item, transitionViewState: newTransitionViewState };
+    const f1 = (target: Point) => (
+      accData: Data,
+      idAndPercentage: IdAndPercentage
+    ) => {
+      const { id, t } = idAndPercentage;
+
+      const newItems: Item[] = accData.items.map(item => {
+        if (!(item.id === id)) {
+          return item;
+        } else {
+          // Too primitive.
+          if (item.transitionViewState.percentage === 1) {
+            // complete.
+            return item;
+          } else {
+            const maxTransitionPercentage = Math.max(
+              item.transitionViewState.percentage,
+              t
+            );
+            const newTransitionViewState: TransitionViewState = {
+              ...item.transitionViewState,
+              position: deltaPoint(target, maxTransitionPercentage),
+              percentage: maxTransitionPercentage
+            };
+            return { ...item, transitionViewState: newTransitionViewState };
+          }
+        }
       });
 
       return {
-        ...data,
+        ...accData,
         items: newItems
       };
     };
 
-    // TODO. abstract more.
-    const f = (data: Data, durationMs: number, targetPoint: Point) => (
-      id: number
-    ): Observable<Data> => {
-      // mapping all items.
-      const data$ = duration$(durationMs).pipe(
+    const durationMs = 1000;
+    const progressOnId = (id: number): Observable<IdAndPercentage> =>
+      duration$(durationMs).pipe(
         map(cubicInOut),
-        map(updateItemPositions(targetPoint)(data))
+        map(t => ({ id: id, t: t }))
       );
 
-      // find in the data, minimal.
-      // const items$ = data.items.map(item => {
-
-      //   if (item.id === id) {
-      //     return duration$(durationMs).pipe(
-      //       map(cubicInOut),
-      //       map(t => ({
-      //         x: distance(target.x)(t),
-      //         y: distance(target.y)(t)
-      //       }))
-      //     )
-      //   } else {
-      //     return EMPTY;
-      //   }
-      // });
-
-      return data$;
-    };
-
-    const target: Point = { x: 400, y: 0 };
-    const durationMs = 300;
-    this.data$ = this.runTransitionSubj.pipe(
-      switchMap(f(initialData, durationMs, target)),
-      startWith(initialData)
+    const s1$ = this.runTransitionSubj.pipe(
+      mergeMap(progressOnId),
+      scan(f1(targetPoint), initialData)
     );
 
-    // const target$ = fromEvent(document, "mousedown").pipe(map(toPoint));
+    // event log.
 
-    // let ball = document.getElementById("ball");
+    const initLogs = { logs: [], counter: 0 };
+    this.logs$ = this.runTransitionSubj.pipe(
+      scan((acc: Logs, id: number): Logs => {
+        const nextLogEntry: LogEntry = { id: id, eventType: "click" };
+        const newLogs: Logs = {
+          counter: acc.counter + 1,
+          logs: [...acc.logs, nextLogEntry]
+        };
+        return newLogs;
+      }, initLogs)
+    );
 
-    // duration in percentage.
-
-    // const s$ = target$.pipe(
-    //   switchMap(({ x, y }) =>
-    //     duration$(durationMs).pipe(
-    //       map(cubicInOut),
-    //       map(t => ({
-    //         x: distance(target.x)(t),
-    //         y: distance(target.y)(t)
-    //       }))
-    //     )
-    //   )
-    // );
-
-    // this.out$ = s$;
-
-    // when done, simply call sub.unsubscribe();
-    // let sub = s$.subscribe(({ x, y }) => {
-    //   ball.style.transform = `translate3d(${x}px, ${y}px, ${0}px)`;
-    // });
+    this.data$ = s1$.pipe(startWith(initialData));
   }
 }
